41c0420c34beaf5d256ebbc40bfe862086279982
Nakayiza Kevina <kevinanakayiza@gmail.com>
2026-03-25 11:36:16 +0300

    installed cors to help connect frontend with backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,17 @@
 import express from 'express'
+import cors from "cors";
 
 const app = express ();
 
 app.use(express.json());
+app.use(cors(
+    {
+  origin: "http://127.0.0.1:5500",
+  methods: ["GET", "POST", "PATCH", "DELETE"],
+  credentials: true
+}
+));
+
 
 import taskRouter from '../routes/task.route.js';
 
